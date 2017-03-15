42121a33e1ad4ba048027381d3c6a882fa8d405e
e2dubba <svirgilgoodwin@gmail.com>
2017-03-15 15:58:47 -0500

    fixed first function

--- src/xlsx.py
+++ src/xlsx.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 import xlrd, xlsxwriter
-import sys, os
+import os, sys
+import subprocess as sp
 
 
-def xlsx2csv(xlfile):
+def xlsx2csv(xlfile, filename):
+    editor = os.environ.get('EDITOR', 'vim -p')
+    dirname = filename.replace('.xlsx', '') + '.vcell'
+    print(dirname)
+    try:
+        os.mkdir(dirname)
+    except FileExistsError:
+        os.chdir(dirname)
+        os.system('vim -p ' + ' '.join(os.listdir('.')))
+        sys.exit()
+    os.chdir(dirname)
+    for sheet in xlfile.sheet_names():
+        csv = open(sheet.replace(' ', '_') + '.csv', 'w')
+        for rw in range(0, xlfile.sheet_by_name(sheet).nrows):
+            values = xlfile.sheet_by_name(sheet).row_values(rw)
+            new_values = ', '.join(values) + '\n'
+            csv.write(new_values)
+        csv.close()
+    print(os.listdir('.'))
+    var = ' '.join(os.listdir('.'))
+    os.system('vim -p ' + var)
+    #sp.Popen(['vim', '-p'] + os.listdir('.'))
 
-
+'''
 def csv2xlsx(directory):
+
+'''
 
 
 def main():
@@ -18,10 +42,13 @@ def main():
     ap.add_argument('-c', '--csv', help='From csv directory to xlsx workbook',
             action='store_true')
     a = ap.parse_args()
+    if a.xlsx:
+        xlsx_filename = a.workbook.pop()
+        xlsxwb = xlrd.open_workbook(xlsx_filename)
+        xlsx2csv(xlsxwb, xlsx_filename)
 
-    
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     import argparse
     main()
